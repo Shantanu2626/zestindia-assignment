c40df01b854b862e0025e10bd5f1bec7e0cbc32c
Shantanu Jape <shantanujape90@gmail.com>
2026-02-19 19:33:31 +0530

    zest project added on repo

---
+++ src/test/java/com/zestindia/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zestindia.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
